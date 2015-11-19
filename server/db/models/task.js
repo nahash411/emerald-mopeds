8c6f8a59a8c39091ee7836fd08982e817d181035
Christian Everett <ceverett@gmail.com>
2015-11-19 14:54:05 -0800

    added routes for getting and posting tasks

--- server/db/models/task.js
+++ server/db/models/task.js
@@ -2,6 +2,7 @@ var db = require('../database');
 var mongoose = require('mongoose');
 
 var taskSchema = mongoose.Schema({
+  user: {type: Number, ref: 'User'},
   job: {type: Number, ref: 'Job'},
   name: Number,
   start: Date,
