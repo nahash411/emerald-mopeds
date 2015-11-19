21beb98d22d3e51cdfeb060c634b5478b90f88db
Christian Everett <ceverett@gmail.com>
2015-11-19 11:32:08 -0800

    added task model

---
+++ server/db/models/task.js
@@ -0,0 +1,15 @@
+var db = require('../database');
+var mongoose = require('mongoose');
+
+var taskSchema = mongoose.Schema({
+ job: {type: Number, ref: 'Job'},
+ name: Number,
+ start: Date,
+ end: Date,
+ status: Boolean
+});
+
+var Task = mongoose.model('Task', jobSchema);
+
+
+module.exports = Task;
