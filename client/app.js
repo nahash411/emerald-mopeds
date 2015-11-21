01cc6eb496d71aef99febe9ee00a521635a742f8
Alex McLean <alexmclean206@berkeley.edu>
2015-11-21 08:52:44 -0800

    built out factories a bit and converted tamplates to html

--- client/app.js
+++ client/app.js
@@ -1,14 +1,17 @@
-var app = angular.module('lancealot', ['lancealot.jobs']);//, 'ngRoute']);
+var app = angular.module('lancealot', ['lancealot.jobs', 'ngRoute']);
 
-// app.config(function ($routeProvider, $httpProvider) {
+app.config(function ($routeProvider, $httpProvider) {
 
-//   $routeProvider
-//     .when('/jobs', {
-//       templateUrl: './jobs.html',
-//       controller: 'JobsController'
-//     });
+  $routeProvider
+    .when('/', {
+      templateUrl: './index.html'
+    })
+    .when('/jobspage', {
+      templateUrl: './jobs.html',
+      controller: 'JobsController'
+    });
 
-// });
+});
 
 app.factory('Jobs', function ($http) {
 
@@ -19,9 +22,39 @@ app.factory('Jobs', function ($http) {
     }).then(function (res) {
       return res.data;
     })
-  }
+  };
 
   return {
     fetchJobs: fetchJobs
+  };
+});
+
+app.factory('Tasks', function ($http){
+  var fetchTasks = function (id) {
+    $http({
+      method: 'GET',
+      url: '/jobs/' + id
+    }).then(function (res){
+      return res.data;
+    });
+  };
+
+  return {
+    fetchTasks: fetchTasks
+  };
+});
+
+app.factory('Clients', function ($http){
+  var fetchClients = function (){
+    $http({
+      method: 'GET',
+      url: '/clients'
+    }).then(function (res){
+      return res.data;
+    });
+  };
+
+  return {
+    fetchClients: fetchClients
   };
 });
